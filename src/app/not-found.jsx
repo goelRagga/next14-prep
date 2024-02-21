dd96918258ca2a042d0ba321061ff1d2722d7c54
Raghav Goel <raghavgoel847@gmail.com>
2024-02-21 10:26:19 +0530

    Routing Setup and Layout setup done

---
+++ src/app/not-found.jsx
@@ -0,0 +1,7 @@
+const NotFound = () => {
+  return (
+    <div>Sorry No Page Found at this route</div>
+  )
+}
+
+export default NotFound
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import Links from "./links/Links"
+
+const Navbar = () => {
+  return (
+    <div>Navbar
+        <Links />
+    </div>
+  )
+}
+
+export default Navbar
